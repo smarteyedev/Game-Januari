50e1f2707a8a7a0179afbca76e11346dff59ea14
mmnur <nurramadan35@gmail.com>
2026-02-11 10:38:17 +0700

    center loading

--- src/components/molecules/GameState.vue
+++ src/components/molecules/GameState.vue
@@ -12,26 +12,17 @@ defineProps<Props>()
 </script>
 
 <template>
-    <div
-v-if="loading"
-class="grid place-items-center">
-        <UiLoading />
-    </div>
+    <div class="flex justify-center items-center h-screen w-screen">
+        <div v-if="loading">
+            <UiLoading class="grid place-items-center" />
+        </div>
 
-    <div
-v-else-if="error"
-class="flex flex-col items-center justify-center gap-4 p-8">
-        <p class="text-h5 text-red-600">Failed to load game</p>
-        <ButtonText
-v-if="retryFn"
-text="Retry"
-variant="primary"
-size="md"
-@click="retryFn" />
-        <p
-v-else
-class="text-body-sm text-gray-500">Please refresh the page</p>
-    </div>
+        <div v-else-if="error" class="flex flex-col items-center justify-center gap-4 p-8">
+            <p class="text-h5 text-red-600">Failed to load game</p>
+            <ButtonText v-if="retryFn" text="Retry" variant="primary" size="md" @click="retryFn" />
+            <p v-else class="text-body-sm text-gray-500">Please refresh the page</p>
+        </div>
 
-    <slot v-else />
+        <slot v-else />
+    </div>
 </template>
