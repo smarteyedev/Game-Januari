dc571af3997aa0bcf993f311e768d60467fb0752
mmnur <nurramadan35@gmail.com>
2026-03-11 08:31:18 +0700

    topnav intro fix

--- src/components/organisms/GameIntroModal.vue
+++ src/components/organisms/GameIntroModal.vue
@@ -39,22 +39,16 @@ const onStart = () => {
 const { isXs, isSm, isMd, buttonSize } = useGameViewContext()
 
 const iconSizeClass = computed(() => {
-  if (isXs.value) return 'w-15 h-15'
-  if (isSm.value) return 'w-18.5 h-18.5'
-  if (isMd.value) return 'w-21 h-21'
-  return 'w-24.5 h-24.5'
+  if (isXs.value) return 'w-12 h-12'
+  if (isSm.value) return 'w-12 h-12'
+  if (isMd.value) return 'w-14 h-14'
+  return 'w-[96px] h-[96px]'
 })
 </script>
 
 <template>
-  <UiModal
-    :container-position="props.containerPosition"
-    :prevent-close="true"
-    :modelValue="modelValue"
-    size="md"
-    position="center"
-    scroll-mode="content"
-    :content-style="{
+  <UiModal :container-position="props.containerPosition" :prevent-close="true" :modelValue="modelValue" size="md"
+    position="center" scroll-mode="content" :content-style="{
       border: '6px solid #006082',
       boxShadow: '0px 8px 0px #006082',
       borderRadius: '40px',
@@ -62,10 +56,7 @@ const iconSizeClass = computed(() => {
       maxHeight: '85vh',
       display: 'flex',
       flexDirection: 'column',
-    }"
-    @update:modelValue="emit('update:modelValue', $event)"
-    @cancel="onClose"
-  >
+    }" @update:modelValue="emit('update:modelValue', $event)" @cancel="onClose">
     <!-- HEADER ICON -->
     <template #header-title>
       <div class="flex justify-center items-center w-full">
@@ -79,23 +70,14 @@ const iconSizeClass = computed(() => {
 
     <!-- BODY -->
     <div class="flex-1 overflow-y-auto px-10 pt-6 flex justify-center">
-      <GameIntro
-        v-if="introData"
-        :title="introData.title"
-        :description="introData.description"
-        :key_points="introData.key_points"
-        class="w-full"
-      />
+      <GameIntro v-if="introData" :title="introData.title" :description="introData.description"
+        :key_points="introData.key_points" class="w-full" />
     </div>
 
     <!-- FOOTER -->
     <template #footer>
       <div class="flex justify-center shrink-0">
-        <UiButton
-text="Mulai Game"
-variant="primary"
-:size="buttonSize"
-@click="onStart" />
+        <UiButton text="Mulai Game" variant="primary" :size="buttonSize" @click="onStart" />
       </div>
     </template>
   </UiModal>
