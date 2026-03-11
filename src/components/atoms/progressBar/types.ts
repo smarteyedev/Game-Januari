a93b78120793a5a312e06db6cba1ba32d537cf8f
mmnur <nurramadan35@gmail.com>
2026-03-11 14:28:25 +0700

    merge progress bar

--- src/components/atoms/progressBar/types.ts
+++ src/components/atoms/progressBar/types.ts
@@ -3,9 +3,13 @@ export interface IProgressBarUi {
   color?: string
 }
 
+export type ProgressBarVariant = 'default' | 'with-icon' | 'with-outline'
+
 export interface IProgressBarProps {
   progress?: number
   max?: number
   showLabel?: boolean
+  showCounter?: boolean
+  variant?: ProgressBarVariant
   ui?: IProgressBarUi
 }
